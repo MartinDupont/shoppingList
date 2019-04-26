f28342ad20dcbbaaa329a607e309930fd2950532
dupontm <martin.dupont@tngtech.com>
2019-04-26 17:33:09 +0200

    got a basic version working

--- app/src/main/java/sample/SampleAndroid.kt
+++ app/src/main/java/sample/SampleAndroid.kt
@@ -2,6 +2,11 @@ package sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.KeyEvent
+import android.view.View
+import android.widget.EditText
 
 actual class Sample {
     actual fun checkMe() = 44
@@ -13,10 +18,51 @@ actual object Platform {
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var editText: EditText
+
+    private val shoppingList = mutableListOf("an item", "another item")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         hello()
         Sample().checkMe()
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = MyAdapter(shoppingList.toTypedArray())
+
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            //setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify a viewAdapter (see also next example)
+            adapter = viewAdapter
+
+
+        }
+
+        editText = findViewById(R.id.editText)
+
+        editText.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+
+                shoppingList.add(editText.text.toString())
+                editText.text.clear()
+                viewAdapter = MyAdapter(shoppingList.toTypedArray())
+                recyclerView.adapter = viewAdapter
+                println("HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                println(shoppingList)
+
+                return@OnKeyListener true
+            }
+            false
+        })
     }
 }
